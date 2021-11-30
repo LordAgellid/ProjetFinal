704961b3118b709ea3ce37ae1dc8ac4d116a7b92
Yao Kounakou <89590606+yaokounakou@users.noreply.github.com>
2021-11-29 20:37:53 -0500

    All fixes added
    - Formulaire de connexion
    - Implémentation du script dans la page de connexion
    - Autres...

--- Client/Public/Pages/Connexion/connexion.js
+++ Client/Public/Pages/Connexion/connexion.js
@@ -1,16 +1,17 @@
 function connexion(event) {
     event.preventDefault();
     let donneeFormulaire = new URLSearchParams(new FormData(event.target));
+    const  error = document.getElementById("error");
 
-    fetch("http://localhost:5000/connexion", {method: "POST", body: donneeFormulaire})
+    fetch("http://localhost:5000/Connexion", {method: "POST", body: donneeFormulaire})
         .then(rep =>{
             return rep.json();
         })
         .then(repJson =>{
-            if (repJson.success){
-                window.location.href = "http://localhost:3000/profilAdmin"
-            }else{
-                alert(repJson.erreur)
+            if (repJson.success) {
+                window.location.href = "http://localhost:3000/Profil/";
+            } else {
+                error.style.display = "block";
             }
         })
 }
--- Serveur/app.js
+++ Serveur/app.js
@@ -1,46 +1,55 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const requete = require("./data-base/requeteKnex.js")
+const requete = require("../Serveur/Database/requeteKnex")
 
 const PORT = process.env.PORT || 5000 ;
 
+/*----------------------------------- READING/LISTENING PORTS -----------------------------------*/
+
 app.use(cors());
+
+app.set('json spaces', 2)
+
 app.use(express.urlencoded({extended: false}))
-//table Profils
-app.get("/profil", async (req, rep) =>{
-    try{
-        table = await requete.tableTableProfil()
+
+/*----------------------------------- PROFILES GET REQUEST -----------------------------------*/
+app.get("/Administrateurs", async (req, rep) =>{
+    try {
+        table = await requete.afficherAdmins()
 
         rep.status(200).json(table)
 
-    }catch(error){
+    } catch(error) {
         rep.status(500).json({
             erreur: error 
         })
     }
 })
 
-// connexion
-app.post("/connexion", async (req, rep) =>{
+/*----------------------------------- CONNEXION POST REQUEST -----------------------------------*/
+
+app.post("/Connexion", async (req, rep) =>{
     try{
-        const table = await requete.tableTableProfil()
+        const table = await requete.afficherAdmins()
         
-        let admin, valide ;
+        let admin, valide;
         
         const info = {
             id: req.body.id,
             password: req.body.password
-        }
+        };
 
-        for (let i = 0; i < table.length; i++) {
+        console.log(info);
+
+        for (i = 0; i < table.length; i++) {
             valide = 0;
             admin = table[0];
 
-            if(info.id === admin.nom_utisateur && info.password === admin.mot_de_passe){
+            if(info.id === admin.Username && info.password === admin.Password) {
                 valide += 1
                 
-                if(!admin.compte_bloque){
+                if(!admin.CompteBloque){
                     valide += 1;
                     break;
                 }
@@ -53,25 +62,23 @@ app.post("/connexion", async (req, rep) =>{
                 compteBloque: false,
                 erreur: "Mot de passe ou nom d'utilisateur invalide "
             })
-        } else if(valide === 1){
+        } else if(valide === 1) {
             rep.status(200).json({
                 success: false,
                 compteBloque: true,
                 erreur: "Compte Bloque"
             })
-        }else{
+        } else {
             rep.status(200).json({success: true})
         }
 
-        
-        
-    }catch(error){
+    } catch(error) {
         rep.status(500).json({
             erreur: error 
         })
     }
 })
 
-
+/*----------------------------------- PORT LISTENER -----------------------------------*/
 
 app.listen(PORT, ()=> console.log("L'application serveur roule sur le port " + PORT))
